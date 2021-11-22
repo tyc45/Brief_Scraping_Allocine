11bceec20e4f22fdaab4efc070dea2bb3f789804
Jordan Tyczynski <tyc450@gmail.com>
2021-11-22 15:20:43 +0100

    initialized main files

---
+++ Brief_Scraping_Allocine/spiders/best_movies.py
@@ -0,0 +1,20 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['www.allocine.fr/film/meilleurs/']
+    start_urls = ['http://www.allocine.fr/film/meilleurs//']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        return item
