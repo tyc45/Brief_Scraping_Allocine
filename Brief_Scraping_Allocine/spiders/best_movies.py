fdc0dbbb12349b377a4de1cfed7fb18c2148b960
Jordan Tyczynski <tyc450@gmail.com>
2021-11-22 16:09:23 +0100

    trying to get my first spider leg working

--- Brief_Scraping_Allocine/spiders/best_movies.py
+++ Brief_Scraping_Allocine/spiders/best_movies.py
@@ -6,14 +6,16 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['www.allocine.fr/film/meilleurs/']
-    start_urls = ['http://www.allocine.fr/film/meilleurs//']
+    start_urls = ['http://www.allocine.fr/film/meilleurs/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow='film/'), callback='parse_item'),
     )
 
     def parse_item(self, response):
-        item = {}
+        item = {
+            'title':response.xpath('//h2/a[@class="meta-title-link"]/text()').getall()
+        }
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
